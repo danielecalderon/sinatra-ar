f291338a238cd5a9241a53f428f534190dc372b2
danielecalderon <daniel.calderon06@gmail.com>
2020-03-18 16:38:43 +0000

    added database

--- db/migrate/20200305005832_create_users.rb
+++ db/migrate/20200305005832_create_users.rb
@@ -1,4 +1,9 @@
-class CreateUsers < ActiveRecord::Migration[6.0]
-  def change
+class CreateUsers < ActiveRecord::Migration[5.2]
+    def change
+      create_table :users do |t|
+        t.string :email
+        t.string :name
+        # t.type :column_name
+      end
+    end
   end
-end
